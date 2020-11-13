9b2477bfc28e5616b4c199a6ebc3a926d15f6605
cat-JS-0610-xyz <72459371+cat-JS-0610-xyz@users.noreply.github.com>
2020-11-13 00:05:17 -0500

    Add files via upload

---
+++ distonary/distonary.py
@@ -0,0 +1,23 @@
+lool = open('dist.txt', 'r')
+d = []
+for stroke in lool:
+    templist = stroke.split()
+    d.append({"name" : templist[0],"surname" : templist[1], "age" : templist[2]})
+e = input("action ")
+while e != "exit":
+    if e == "new":
+        w = input("name ")
+        q = input("surname ")
+        a = input("age ")
+        d.append({"name" : w,"surname" : q, "age" : a})
+    if e == "show":
+        j = int(input("plase "))
+        k = input("what ")
+        print(d[j][k])
+    if e == "save":
+        lool.close()
+        lool = open('dist.txt', 'w')
+        for dict in d:
+            print(dict["age"])
+    e = input("action ")
+lool.close()
